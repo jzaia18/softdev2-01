11b5d3be678ea00870dbe5bc3439f1b5c8ec3000
Jake Zaia <jzaia@stuy.edu>
2018-02-08 14:50:26 -0500

    Add DVD bounce animation

--- circle.js
+++ circle.js
@@ -7,9 +7,8 @@ var clear = function() {
   ctx.beginPath();
 };
 
-var animate = function() {
-  //special
-  window.cancelAnimationFrame(reqID);
+var animateCircle = function() {
+  stophammertime();
 
   ctx.fillStyle = "lightsteelblue";
 
@@ -24,25 +23,60 @@ var animate = function() {
 
     if (rad > ctx.canvas.width/2)
       radMode = 1;
-    if (rad <= 0)
+    if (rad <= 1)
       radMode = 0;
 
     if (radMode)
-      rad--;
+      rad-=2;
     else
-      rad++;
+      rad+=2;
     reqID = window.requestAnimationFrame(drawCircle);
-    console.log("ReqID: " + reqID);
+    if (reqID%10 == 0)
+      console.log("ReqID: " + reqID);
   };
 
-  //special
   drawCircle();
+};
+
+var animateDVD = function() {
+  stophammertime();
+
+  var dx = Math.floor(Math.random() * 5 + 1);
+  var dy = Math.floor(Math.random() * 5 + 1);
+  var x = Math.floor(Math.random() * (ctx.canvas.width - img.width*2) + img.width + 1);
+  var y = Math.floor(Math.random() * (ctx.canvas.height - img.height*2) + img.height + 1);
+
+
+
+  var drawDVD = function() {
+    clear();
+    ctx.drawImage(img,x,y);
+
+    reqID = window.requestAnimationFrame(drawDVD);
+    if (reqID%10 == 0)
+      console.log("ReqID: " + reqID);
+
+    if (Math.abs(y-ctx.canvas.height) < img.height + 1 || y < 1)
+      dy*=-1;
+    if (Math.abs(x-ctx.canvas.width) < img.width + 1 || x < 1)
+      dx*=-1;
+
+
+    x+=dx;
+    y+=dy;
+  };
+
+  drawDVD();
 };
 
 var ctx = document.getElementById("iamthecanvas").getContext('2d');
 var TAU = 2*Math.PI; //TAU!!!
 var reqID = 0;
 
-document.getElementById("iamthecanvas").addEventListener("click", animate);
+var img = new Image();
+img.src="david.png";
+
+document.getElementById("circle").addEventListener("click", animateCircle);
+document.getElementById("dvd").addEventListener("click", animateDVD);
 document.getElementById("stop").addEventListener("click", stophammertime);
 document.getElementById("clear").addEventListener("click", clear);
